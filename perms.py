222957fa3d70a988c31b278bca73987296c5ec11
david <dasmartins1234@gmail.com>
2021-07-12 12:20:00 +0100

    Permissões por nivel

--- perms.py
+++ perms.py
@@ -3,11 +3,43 @@ from flask import flash, redirect, url_for
 from flask_login import current_user
 
 
-def roles(*roles):
+ACCESS = {
+    'Cliente': 0,
+    'User': 1,
+    'Super': 2,
+    'Gest': 3,
+    'Admin': 99
+}
+def roles(role):
     def wrapper(f):
         @wraps(f)
         def wrapped(*args, **kwargs):
-            if current_user.role not in roles:
+            if not ACCESS[current_user.role] > ACCESS[role]:
+                # Redirect the user to an unauthorized notice!
+                flash("Não tem permissão para aceder a esta pagina!", category="error")
+                return redirect(url_for('views.dashboard'))
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
+
+# Admin>Gerente>Supervisor>Operador
+from functools import wraps
+from flask import flash, redirect, url_for
+from flask_login import current_user
+
+
+ACCESS = {
+    'Cliente': 0,
+    'User': 1,
+    'Super': 2,
+    'Gest': 3,
+    'Admin': 99
+}
+def roles(role):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if not ACCESS[current_user.role] >= ACCESS[role]:
                 # Redirect the user to an unauthorized notice!
                 flash("Não tem permissão para aceder a esta pagina!", category="error")
                 return redirect(url_for('views.dashboard'))
